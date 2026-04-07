451921e4d6f80e81fca3788bfa89dfac4a2ec8a0
akshmeett <akshmeetk@gmail.com>
2026-04-07 20:17:09 +0530

    office sys: Test class with diff int data types

---
+++ test.java
@@ -0,0 +1,19 @@
+// steps to update code:
+// git add .     [git tracks the changes]
+// git commit -m "message"   [saves version locally with the changes]
+// git push      [uploads changes to git]
+// git pull [pulls latest code from git]
+//git status [shows the list of changes in the current dir]
+
+public class Test{
+
+    public static void main(String[] args){
+        int age = 20;
+        byte age1 = 20;
+        short age2 = 20;
+        long age3 = 20;
+
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+    }
+}
