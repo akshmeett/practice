61ef59dfdc2bf6e1616605e039f4278db0484921
akshmeett <akshmeetk@gmail.com>
2026-04-08 16:24:35 +0530

    office sys: done till vid 7

--- test.java
+++ test.java
@@ -8,12 +8,37 @@
 public class Test{
 
     public static void main(String[] args){
-        int age = 20;
-        byte age1 = 20;
-        short age2 = 20;
-        long age3 = 20;
+        // int age = 20;
+        // byte age1 = 20;
+        // short age2 = 20;
+        // long age3 = 20;
 
-        System.out.println(Long.MIN_VALUE);
-        System.out.println(Long.MAX_VALUE);
+        // System.out.println(Long.MIN_VALUE);
+        // System.out.println(Long.MAX_VALUE);
+
+        // float mySalary = 1000.102828283829f;
+        // double mySalary2 = 1000.102828283829;
+
+        // System.out.println(mySalary); 
+        // System.out.println(mySalary2);
+
+        // char initial = 'z';
+        // System.out.println((int) Character.MIN_VALUE);
+        // System.out.println((int) Character.MAX_VALUE);
+
+        // System.out.println("❤️");
+
+        // int yourSalary = 1000;
+        // int deduction = 300;
+        // int monthlyTotal = yourSalary - deduction;
+        // int yearlyTotal = monthlyTotal * 12;
+        // int totalPerChild = yearlyTotal/3;
+
+        // int a = 10;
+        // int b = 20;
+        // int remainder = a % b;
+        // System.out.println(remainder);
+
+        
     }
-}
+} 
